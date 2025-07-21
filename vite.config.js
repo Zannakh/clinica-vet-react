b35a67faf7f3cbdd1698c8e6c9f92a2fc7251ca7
Zannakh <flarranagad@gmail.com>
2025-07-21 00:39:45 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-    historyApiFallback: true
-  }
+  base: '/clinica-vet-react/', // ← Nombre exacto del repositorio de GitHub
+  plugins: [react()]
 })
